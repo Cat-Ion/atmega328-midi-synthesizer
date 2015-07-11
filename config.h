f139c5ca12b051f16d88808b166e730c159df2a6
Valentin Ochs <a@0au.de>
2015-07-11 21:01:42 +0200

    Use 5 oscillators

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
-#define N_OSC 1
+#define N_OSC 5
 #define N_SAMP 256
 #define HIGHEST_OCTAVE 10
 #define VOLUME_TRANSITION 1 /* If non-zero, volume changes by at most one per wave period */
